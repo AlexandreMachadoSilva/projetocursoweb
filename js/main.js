733eb5e525a5ec953da39dbfceddff2c6aa30589
AlexandreMachadoSilva <leuel@msn.com>
2021-08-17 20:40:02 -0300

    novo

---
+++ js/main.js
@@ -0,0 +1,39 @@
+$('.your-class').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slideToShow: 1,
+    adaptiveHeight: true,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    fade: true,
+    cssEase: 'linear',
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
